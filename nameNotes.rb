5dc44afec833721ea52bdad673fc5bbde234185c
meads58 <meads109@gmail.com>
2014-11-26 20:55:56 +0000

    prototype created and working

---
+++ nameNotes.rb
@@ -0,0 +1,33 @@
+#what note is on the fret x, of string x?
+
+noteHash = Hash.new
+
+@noteHash = {61 =>"F", 62 =>"F#", 63 =>"G"}
+
+
+
+
+#selects and random string and fret combination
+def randomPosition
+ string = 6 #rand(6) + 1
+ fret = rand(3) + 1
+ postion = (string.to_s + fret.to_s).to_i
+ 
+ #puts "What is the note on the #{fret} fret of the #{string} string?"
+ 
+ return postion
+ 
+end
+
+def checkAnswer ans
+
+    if @noteHash.has_key?(ans) then
+        puts @noteHash[ans]
+    end
+
+end
+
+
+
+noteQuestion = randomPosition()
+checkAnswer(noteQuestion)
